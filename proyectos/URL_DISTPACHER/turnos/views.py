a30e5ca21acebcac611e4323174b527583c9f089
Lucas Quiroga <ing.lucasquiroga@outlook.com.ar>
2023-05-03 21:24:33 -0300

    Simulacion andando

--- proyectos/URL_DISTPACHER/turnos/views.py
+++ proyectos/URL_DISTPACHER/turnos/views.py
@@ -1,40 +1,62 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse
-#from .models import Profile #podria ser paciente, este se necesita para visuaisar cual es el paciente que hizo el log-in
-from .models import Especialidad #para la lista desplegable
+
+# from .models import Profile #podria ser paciente, este se necesita para visuaisar cual es el paciente que hizo el log-in
+from .models import Especialidad  # para la lista desplegable
 from .models import Appointment
+
 # Create your views here.
+
 
 def index(request):
     return render(request, "index.html")
 
-#def turnos(request):
-#    return render(request, "turnos.html")
+
+# def turnos(request):
+#     return render(request, "turnos.html")
+
 
 def ingreso(request):
-    return render(request, "login.html",  {})
+    return render(request, "login.html", {})
+
 
 def registro(request):
     return render(request, "reg_usuario.html")
 
-#def misturnos(request):
+
+# def misturnos(request):
 #    return render(request, "reg_usuario.html")
+
 
 def turnostodos(request):
     return HttpResponse("Todos los turnos")
 
+
 def turnosporprofesional(request):
     return HttpResponse("Turnos por profesional")
+
 
 def turnosporespecialidad(request):
     return HttpResponse("Turnos por especialidad")
 
+
 def turnospordni(request, dni):
     return HttpResponse(f"<h1>Listado de turnos para el dni n° {dni}</h1>")
 
+
 def contact(request):
     return HttpResponse("Formulario de registro")
+
+
+# --------------------------------------------------------------------
+def paciente(request, nombre):
+    context = {"nombre": nombre}
+    return render(request, "paciente.html", {"context": context})
+    # return HttpResponse("hola")
+
+
+# --------------------------------------------------------------------
 
 # def my_view(request): #para que se visualicen esos datos en la parte de turnos
 #     user = request.user
@@ -49,22 +71,21 @@ def contact(request):
 #     especialidad = Especialidad.objects.all()
 #     return render(request, 'turnos.html', {'especialidad': especialidad})
 
-def turnos(request): #ESTO E PARA QUE SE VISUALICE LA ESPECIALIDAD
+
+def turnos(request):  # ESTO E PARA QUE SE VISUALICE LA ESPECIALIDAD
     especialidades = {
-        'cardiologia': 'Cardiología',
-        'dermatologia': 'Dermatología',
-        'endocrinologia': 'Endocrinología',
-        'gastroenterologia': 'Gastroenterología',
-        'neurologia': 'Neurología',
-        'oncologia': 'Oncología',
+        "cardiologia": "Cardiología",
+        "dermatologia": "Dermatología",
+        "endocrinologia": "Endocrinología",
+        "gastroenterologia": "Gastroenterología",
+        "neurologia": "Neurología",
+        "oncologia": "Oncología",
     }
-
-    return render(request, 'turnos.html', {'especialidades': especialidades}) 
-
+    
+    return render(request, "turnos.html", {"especialidades": especialidades})
 
 
 # def appointment_calendar(request, especialidad): #esto e para el calendar
 #     appointments = Appointment.objects.filter(especialidad=especialidad)
 #     context = {'appointments': appointments}
 #     return render(request, 'turnos.html', context)
-
