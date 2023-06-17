a3b02d61d77d7565a95ec823c7e5ac8438f65ff8
belenserrapio <113253059+Rockbell33@users.noreply.github.com>
2023-06-17 15:08:05 -0300

    Delete URL_DISTPACHER directory

--- URL_DISTPACHER/turnos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from turnos.models import Persona, Medico, Paciente, Turno, Especialidad
-
-# Register your models here.
-admin.site.register(Persona)
-admin.site.register(Medico)
-admin.site.register(Paciente)
-admin.site.register(Turno)
-admin.site.register(Especialidad)
